46ad031c8e4595be5f8cc9cbd6c92d4f178d7245
Teerasej <teerasej@gmail.com>
2017-12-14 10:40:13 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/hello', function(){
+    echo 'ว่าไง';
+});
+
+$app->get('/user/register', function(){ echo '/user/register'; });
+$app->get('/room /all', function(){ echo '/room/all'; });
+$app->get('/room/reserve', function(){ echo '/room/reserve'; });
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
